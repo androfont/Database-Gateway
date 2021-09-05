dfa337eff8dd26533fd517eac0666a692e87d4da
Andro Font <androfont@gmail.com>
2021-09-05 10:44:42 -0300

    Add project files.

---
+++ DatabaseGateway.RestApi/Controllers/PublicQueryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseGateway.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicQueryController : ControllerBase
+    {
+    }
+}
